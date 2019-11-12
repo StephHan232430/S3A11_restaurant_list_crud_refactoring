8dfbffdefbadf0c7b13d1ed59aaeec3829bb1911
Steph <blackman914037@gmail.com>
2019-11-13 01:09:37 +0800

    Finish index action and page

--- app.js
+++ app.js
@@ -31,9 +31,48 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'main' }))
 app.set('view engine', 'hbs')
 
-// 設定首頁路由
+// 設定路由
+// restaurant首頁
 app.get('/', (req, res) => {
-  res.render('index')
+  return res.redirect('/restaurants')
+})
+
+// 列出全部restaurant
+app.get('/restaurants', (req, res) => {
+  Restaurant.find((err, restaurants) => {
+    if (err) return console.error(err)
+    return res.render('index', { restaurants })
+  })
+})
+
+// 新增一間restaurant頁面
+app.get('/restaurants/new', (req, res) => {
+  res.send('新增restaurant頁面')
+})
+
+// 新增一間restaurant
+app.post('/restaurants', (req, res) => {
+  res.send('建立restaurant')
+})
+
+// 顯示一間restaurant的詳細資訊
+app.get('restaurants/:id', (req, res) => {
+  res.send('顯示restaurant的詳細內容')
+})
+
+// 修改restaurant頁面
+app.get('/restaurants/:id/edit', (req, res) => {
+  res.send('修改restaurant頁面')
+})
+
+// 修改restaurant
+app.post('restaurants/:id/edit', (req, res) => {
+  res.send('修改restaurant')
+})
+
+// 刪除restaurant
+app.post('/restaurants/:id/delete', (req, res) => {
+  res.send('刪除restaurant')
 })
 
 // 啟動並監聽伺服器
